adb5fbeaa3b582bb4d55def8adfb6e9edb1b327b
TheOctan <kanatush.tim@gmail.com>
2020-10-14 09:42:36 +0300

    init

---
+++ Assets/Trigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trigger : MonoBehaviour
+{
+	public Text toggleText;
+
+	public void OnToggleTrigger(bool isOn) 
+	{
+		switch (isOn)
+		{
+			case true:
+				toggleText.text = "Yes";
+				break;
+
+			case false:
+				toggleText.text = "No";
+				break;
+		}
+	}
+}
