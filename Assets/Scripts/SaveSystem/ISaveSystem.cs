058b3d84aaae51cfdfca64716d065bd157420267
TheOctan <kanatush.tim@gmail.com>
2020-10-15 17:32:17 +0300

    save system interface

---
+++ Assets/Scripts/SaveSystem/ISaveSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.SaveSystem
+{
+	public interface ISaveSystem
+	{
+		bool SaveObject<T>(T obj);
+		bool SaveObject<T>(T obj, string key);
+		T LoadObject<T>(T obj, string key);
+	}
+}
