7b07c860b5f3cac07b4d831c77c891941da54304
TheOctan <kanatush.tim@gmail.com>
2020-10-18 00:18:53 +0300

    add question column

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public RectTransform[] childs;
+
+    void Start()
+    {
+		foreach (var item in childs)
+		{
+            Debug.Log(item.GetSiblingIndex());
+		}
+    }
+
+}
---
+++ Assets/Scripts/Context/ContentManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContentManager: MonoBehaviour
+{
+    [Header("Prefabs")]
+    public GameObject ObjectColumnPrefab;
+    public GameObject QuestionColumnPrefab;
+    public GameObject AddColumnPrefab;
+
+    [Header("Columns")]
+    public RectTransform ObjectColumn;
+    public RectTransform AddColumn;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnAddColumnButtonClick()
+	{
+		var column = Instantiate(QuestionColumnPrefab, transform);
+        SwapChildObjects(AddColumn, column.transform);
+	}
+
+    private void SwapChildObjects(Transform left, Transform right)
+	{
+        var leftIndex = left.transform.GetSiblingIndex();
+		right.transform.SetSiblingIndex(leftIndex);
+	}
+
+    public void OnAddRowButtonClick()
+	{
+
+	}
+
+    public void OnRemoveColumnButtonClick(int index)
+	{
+
+	}
+
+    public void OnRemoveRowButtonClick(int index)
+	{
+
+	}
+}
