1b2bd38e99f1aab012b9fb18c8e3efb37e41c0c2
TheOctan <kanatush.tim@gmail.com>
2020-10-20 15:46:30 +0300

    save system

--- Assets/Scripts/SaveSystem/SaveSystem.cs
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -2,6 +2,7 @@
 using Assets.Scripts.SaveSystem.Serialization.Format;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace Assets.Scripts.SaveSystem
 {
 	public class SaveSystem
 	{
-		private static ISerializationFileSystem serializationFileSystem = new JsonSerializationSystem(Application.persistentDataPath + "/Saves");
+		private static ISerializationFileSystem serializationFileSystem = new JsonSerializationSystem(Application.persistentDataPath + "/Saves/");
 
 		public static bool Save<T>(string saveName, T saveData)
 		{
@@ -28,6 +29,29 @@ namespace Assets.Scripts.SaveSystem
 				Debug.LogError($"Failed to load file at {path}");
 				return default;
 			}
+		}
+		public static bool HasKey(string key)
+		{
+			return File.Exists(serializationFileSystem.DirectoryName + key + serializationFileSystem.Extension);
+		}
+		public static bool ResetSave(string key)
+		{
+			var file = new FileInfo(serializationFileSystem + key + serializationFileSystem.Extension);
+			file.Delete();
+
+			return true;
+		}
+		public static bool ResetAllSaves()
+		{
+			var directory = new DirectoryInfo(serializationFileSystem.DirectoryName);
+			directory.Delete();
+			Directory.CreateDirectory(serializationFileSystem.DirectoryName);
+
+			return true;
+		}
+		public static void SetSerializationSystem(ISerializationFileSystem serializationFileSystem)
+		{
+			SaveSystem.serializationFileSystem = serializationFileSystem;
 		}
 		public static void SetSaveDirectory(string directory)
 		{
--- Assets/Scripts/SaveSystem/Serialization/ISerializationFileSystem.cs
+++ Assets/Scripts/SaveSystem/Serialization/ISerializationFileSystem.cs
@@ -8,6 +8,7 @@ namespace Assets.Scripts.SaveSystem.Serialization
 {
 	public interface ISerializationFileSystem : ISerializationSystem
 	{
+		string Extension { get; }
 		string DirectoryName { get; set; }
 
 		bool SerializeObject<T>(T obj, string key);
