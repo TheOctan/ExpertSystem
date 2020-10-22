5e6638ecf2130c188763ea612d7edcfc3cfb15e5
TheOctan <kanatush.tim@gmail.com>
2020-10-22 15:46:47 +0300

    reset and menu buttons handling

---
+++ Assets/Scripts/MenuScripts/GameMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMenu : MonoBehaviour
+{
+	public Text questionText;
+
+	public void AnserButtonClick(bool answer)
+	{
+
+	}
+
+	public void ResetButtonClick()
+	{
+
+	}
+}
